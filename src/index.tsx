e39d749e4b2de55e217f2d87443246508dc7ed76
iseyoshitaka <ise0615@gmail.com>
2022-03-27 18:17:23 +0900

    DevToolsとFortawesomeアイコンの導入

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,35 @@ import './styles/main.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
+
+import reducers from './store/Store'
+
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+library.add(fab, far, fas)
+
+// 開発環境の場合は、redux-devtools-extension を利用できるようにする
+const enhancer =
+    process.env.NODE_ENV === 'development'
+        ? composeWithDevTools(applyMiddleware(thunk))
+        : applyMiddleware(thunk)
+const store = createStore(reducers, enhancer)
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <MuiThemeProvider>
+        <React.StrictMode>
+            <Provider store={store}>
+                <App />
+            </Provider>
+        </React.StrictMode>
+    </MuiThemeProvider>,
   document.getElementById('root')
 );
 
